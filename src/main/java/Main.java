7109100db1a453678dfa97d29921688af0aa7195
ShopyK4 <1108564336@u.icesi.edu.co>
2026-03-16 18:52:14 -0500

    :)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,11 +7,35 @@ public class Main {
     private static BronzeExcercises bronce = new BronzeExcercises();
 
     public static void main(String[] args) {
+
+        cinema();
+
+    }
+
+    public void footBall() {
         System.out.println("Ingrese la cadena de texto");
         String secuence = sc.nextLine();
         sc.close();
-
         bronce.footBall(secuence);
+    }
+
+    public static void chatRoom() {
+        System.out.println("ingrese la cadena de texto");
+        String word = sc.nextLine();
+        sc.close();
+        bronce.chatRoom(word);
+    }
+
+    public static void cinema() {
+        int n = sc.nextInt();
+
+        int[] cine = new int[n];
+        for (int i = 0; i < n; i++) {
+            cine[i] = sc.nextInt();
+        }
+
+        bronce.cinema(cine);
+
 
     }
 }
--- src/main/java/bronze/BronzeExcercises.java
+++ src/main/java/bronze/BronzeExcercises.java
@@ -26,4 +26,61 @@ public class BronzeExcercises {
             System.out.println("NO");
         }
     }
+
+    public void chatRoom(String word) {
+        int i = 0;
+        int j = 0;
+        char[] target = {'h', 'e', 'l', 'l', 'o'};
+
+        int result = 0;
+        while (i < word.length()) {
+
+            if (word.charAt(i) == target[j]) {
+                j++;
+                result++;
+                if (result == 5) {
+                    System.out.println("SI");
+                    break;
+                }
+            }
+            i++;
+        }
+        if (result < 5) {
+            System.out.println("NO");
+        }
+    }
+
+    public void cinema(int[] arr) {
+        int devuelta = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 25) {
+                devuelta += 25;
+            }
+            if (arr[i] == 50) {
+                if (devuelta >= 25) {
+                    devuelta -= 25;
+                }
+                else {
+                    System.out.println("no hay devuelta");
+                    devuelta = -1;
+                    break;
+                }
+            }
+            if (arr[i] == 100) {
+                if (devuelta >= 75) {
+                    devuelta += 25;
+                    devuelta -= 75;
+                }
+                else {
+                    System.out.println("no hay devuelta");
+                    devuelta = -1;
+                    break;
+                }
+            }
+        }
+        if (devuelta >= 0) {
+            System.out.println("YES");
+        }
+    }
 }
